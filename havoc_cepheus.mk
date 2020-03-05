b1a7f5fba69471cddc63fe8eb972457045a11bfc
Akash <akashniki@gmail.com>
2020-03-05 18:38:44 +0000

    V11.0.8

--- havoc_cepheus.mk
+++ havoc_cepheus.mk
@@ -16,10 +16,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/V11.0.7.0.QFAMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/V11.0.7.0.QFAEUXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cepheus-user 10 QKQ1.190825.002 V11.0.7.0 release-keys" \
+    PRIVATE_BUILD_DESC="cepheus-user 10 QKQ1.190825.002 V11.0.8.0.QFAEUXM release-keys" \
     PRODUCT_NAME="cepheus" \
     TARGET_DEVICE="cepheus"
 
