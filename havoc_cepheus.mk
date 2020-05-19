159096cb1771c2311fae715b83c5c3ef5b356b18
savsir <svsrcnkt@gmail.com>
2020-05-19 19:18:53 +0300

    No need FP here

--- havoc_cepheus.mk
+++ havoc_cepheus.mk
@@ -17,10 +17,7 @@ PRODUCT_MODEL := MI 9
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_FACE_UNLOCK := true
 
-BUILD_FINGERPRINT := "Xiaomi/cepheus/cepheus:10/QKQ1.190825.002/V11.0.8.0.QFAEUXM:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cepheus-user 10 QKQ1.190825.002 V11.0.8.0.QFAEUXM release-keys" \
     PRODUCT_NAME="cepheus" \
     TARGET_DEVICE="cepheus"
 
